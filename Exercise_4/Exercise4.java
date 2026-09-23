5f14560ccb948c7114047e1703d6c60c4d43c9ed
Mohammed Boukham <mohammedboukhammail@gmail.com>
2026-09-23 15:01:33 +0000

    finished exercise 4

---
+++ Exercise_4/Exercise4.java
@@ -0,0 +1,12 @@
+public class Exercise4{
+    public static boolean teenNumberChecker(int x,int y,int z){
+        if(13<=x && x<=19|| 13<=y && y<=19 || 13<=z && z<=19){return true;}
+        else{return false;}
+    }
+    public static void main(String[] args){
+        int x=1;
+        int y=1;
+        int z=1;
+        System.out.println(teenNumberChecker(x,y,z));
+    }
+}
